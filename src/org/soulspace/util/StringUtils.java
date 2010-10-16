eb62ae9fb1fcb7fe1cf1cf5dcb11f1aebc73ca3d
Ludger Solbach <soulman@soulspace.org>
2010-10-16 19:33:20 +0000

    enhanced string utils

--- src/org/soulspace/util/StringUtils.java
+++ src/org/soulspace/util/StringUtils.java
@@ -1,5 +1,12 @@
 package org.soulspace.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class StringUtils {
 
 	/**
@@ -19,4 +26,35 @@ public class StringUtils {
 	public static String toString(Object o) {
 		return (o != null) ? o.toString() : "";
 	}
+	
+	public static String loadStringFromFile(String fileName) throws IOException {
+		StringBuilder buffer = new StringBuilder(128);
+		String line = null;
+
+		File file = new File(fileName);
+		System.out.println(file.getAbsolutePath());
+
+		// loadDir(file.getParent());
+
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		while ((line = in.readLine()) != null) {
+			buffer.append(line + "\n");
+		}
+		in.close();
+		return buffer.toString();
+	}
+
+	public static String loadStringFromStream(InputStream is)
+			throws IOException {
+		StringBuilder buffer = new StringBuilder(128);
+		String line = null;
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(is));
+		while ((line = in.readLine()) != null) {
+			buffer.append(line + "\n");
+		}
+		in.close();
+		return buffer.toString();
+	}
+
 }
