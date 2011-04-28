5aa5b6bba8944da9f4a112ff2e7bec92cc8f23e5
Ludger Solbach <soulman@soulspace.org>
2011-04-28 21:55:19 +0000

    added Versioned

--- src/org/soulspace/annotation/domain/Versioned.java
+++ src/org/soulspace/annotation/domain/Versioned.java
@@ -1,12 +1,13 @@
 package org.soulspace.annotation.domain;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface Repository {
+public @interface Versioned {
 
 }
