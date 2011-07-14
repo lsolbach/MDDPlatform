c1abe9b03bd1f538e14cc34ca878170966dacdcc
Ludger Solbach <soulman@soulspace.org>
2011-07-14 18:01:57 +0000

    annotation enhancements

--- src/org/soulspace/annotation/domain/AggregateRoot.java
+++ src/org/soulspace/annotation/domain/AggregateRoot.java
@@ -2,16 +2,18 @@ package org.soulspace.annotation.domain;
 
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation for objects that are the root of en Aggregate
+ * Annotation for objects that are the root of an Aggregate
  * @author soulman
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface AggregateRoot {
 }
