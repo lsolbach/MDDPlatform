44f78e75817f89402a801f14560b7af417ffe0d8
Ludger Solbach <soulman@soulspace.org>
2012-11-21 07:50:19 +0000

    added module.clj

---
+++ module.clj
@@ -0,0 +1,10 @@
+[
+ :name "JavaUtilLibrary"
+ :project "org.soulspace"
+ :type "framework"
+ :version "0.2.0"
+ :description "The JavaUtilLibrary is a library of utility classes with pure java convenience methods."
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "package"]
+ :dependencies [["org.junit" "junit" "3.8.1" "dev"]]
+ ]
