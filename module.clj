8fd12708327397c70add991a6b415f94e60d77f4
Ludger Solbach <soulman@soulspace.org>
2012-11-28 08:58:13 +0000

    version bump to 0.3.0, updated project files

--- module.clj
+++ module.clj
@@ -3,12 +3,10 @@
  :project "org.soulspace"
  :project-lead "Ludger Solbach"
  :vendor "soulspace.org"
- :type "framework"
- :version "0.2.0"
+ :type "library"
+ :version "0.3.0"
  :description "The JavaUtilLibrary is a library of utility classes with pure java convenience methods."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "deps" "java" "package"]
- :dependencies [
-                ["junit" "junit" "3.8.1" "dev"]
-                ]
+ :dependencies [["junit" "junit" "3.8.1" "dev"]]
  ]
--- readme.txt
+++ readme.txt
@@ -7,8 +7,17 @@ License
 -------
 Eclipse Public License 1.0 (http://www.eclipse.org/legal/epl-v10.html)
 
+Project Lead
+------------
+Ludger Solbach
+
 History
 -------
+
+Version 0.3.0 (--.--.2012)
+--------------------------
+* refactorings
+* enhanced unit tests
 
 Version 0.2.0 (--.--.201-)
 --------------------------
