527b4c9349a6f6bda2e2d20681ea3af6ca334bed
Ludger Solbach <soulman@soulspace.org>
2012-11-30 16:40:48 +0000

    updated project files

---
+++ module.clj
@@ -0,0 +1,11 @@
+[
+ :name "AnnotationLibrary"
+ :project "org.soulspace.architecture"
+ :project-lead "Ludger Solbach"
+ :type "library"
+ :version "0.3.1"
+ :description "A java annotation library to support model driven and aspect oriented architectures."
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "package"]
+ :dependencies []
+ ]
--- readme.txt
+++ readme.txt
@@ -1,8 +1,14 @@
-Annotation Library
+AnnotationLibrary
+=================
 
-Annotations for the Soulspace Architecture
+A java annotation library to support model driven and aspect oriented architectures
+
+License
+-------
+
+History
+-------
 
 Version 0.3.1
 -------------
 Added Tracked annotation
-
