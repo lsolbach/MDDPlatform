8b9bd9f4513dfb5013b716250d0ba617c2a267e8
Ludger Solbach <soulman@soulspace.org>
2012-11-30 16:44:29 +0000

    updated module.clj

---
+++ module.clj
@@ -0,0 +1,16 @@
+[
+ :name "AspectLibrary"
+ :project "org.soulspace.architecture"
+ :project-lead "Ludger Solbach"
+ :type "library"
+ :version "0.2.0"
+ :description "An aspect library containing some useful general purpose aspects"
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "aspectj" "junit" "package"]
+ :dependencies [["org.soulspace.architecture" "AnnotationLibrary" "0.3.1"]
+                ["org.aspectj" "aspectjrt" "1.6.11"] ; transitive AspectLibrary
+                ["log4j" "log4j" "1.2.15" "dev"]
+                ["commons-logging" "commons-logging" "1.1.1" "dev"]
+                ["junit" "junit" "3.8.1" "dev"]
+                ]
+ ]
