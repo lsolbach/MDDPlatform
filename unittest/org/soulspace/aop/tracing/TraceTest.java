3f4a51318e24033c89913be8e0cbc45b7485dd87
Ludger Solbach <soulman@soulspace.org>
2012-12-05 08:38:30 +0000

    added dummy TraceTest

---
+++ unittest/org/soulspace/aop/tracing/TraceTest.java
@@ -0,0 +1,15 @@
+package org.soulspace.aop.tracing;
+
+import junit.framework.TestCase;
+
+public class TraceTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
