4d4091ced5940a25031f7df38e3f4054768f602f
Ludger Solbach <soulman@soulspace.org>
2012-12-06 08:55:31 +0000

    renamed module to ArchitectureFramework, updated project files

---
+++ module.clj
@@ -0,0 +1,14 @@
+[
+ :name "ArchitectureFramework"
+ :project "org.soulspace.architecture"
+ :project-lead "Ludger Solbach"
+ :type "framework"
+ :version "0.2.0"
+ :description "An architecture framework with abstractions and aspects"
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "aspectj" "junit" "package" "publish"]
+ :dependencies [["org.soulspace.architecture" "AspectLibrary" "0.2.0" "aspect"]
+                ["org.soulspace.architecture" "AnnotationLibrary" "0.3.1"]
+                ["org.aspectj" "aspectjrt" "1.6.11"] ; transitive AspectLibrary
+                ["junit" "junit" "3.8.1" "dev"]]
+ ]
