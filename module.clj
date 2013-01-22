ba24f5ad84e80efbce262bad9544b39d4cecf397
Ludger Solbach <soulman@soulspace.org>
2013-01-22 20:23:38 +0100

    initial git import

---
+++ module.clj
@@ -0,0 +1,12 @@
+[
+ :name "JpaLibrary"
+ :project "org.soulspace.modelling"
+ :type "library"
+ :version "0.3.0"
+ :description "A repository for UML 1.4/XMI 1.2 files"
+ :vendor "soulspace.org"
+ :plugins ["global" "deps" "java" "package"]
+ :dependencies [["org.soulspace.architecture" "AspectLibrary" "0.2.0"]
+                ["org.soulspace.architecture" "AnnotationLibrary" "0.3.1"]
+                ["junit" "junit" "3.8.1" "dev"]]
+ ]
