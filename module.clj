a56114821d63acf4f30c5c399f0562d60ddac03e
Ludger Solbach <soulman@soulspace.org>
2013-03-08 18:05:36 +0100

    refactored ant build, bumped version to 0.4.0

--- module.clj
+++ module.clj
@@ -3,7 +3,7 @@
  :project "org.soulspace.architecture"
  :project-lead "Ludger Solbach"
  :type "library"
- :version "0.3.1"
+ :version "0.4.0"
  :description "A java annotation library to support model driven and aspect oriented architectures."
  :vendor "soulspace.org"
  :plugins ["global" "deps" "java" "package"]
