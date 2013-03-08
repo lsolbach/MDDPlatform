404e17470b2d93620ab471aeb4ca3e06a2c3aa74
Ludger Solbach <soulman@soulspace.org>
2013-03-08 18:00:02 +0100

    refactored ant build, removed unfinished test, bumped version to 0.4.0

--- module.clj
+++ module.clj
@@ -7,7 +7,7 @@
  :description "An aspect library containing some useful general purpose aspects"
  :vendor "soulspace.org"
  :plugins ["global" "deps" "aspectj" "junit" "package"]
- :dependencies [["org.soulspace.architecture" "AnnotationLibrary" "0.3.1"]
+ :dependencies [["org.soulspace.architecture" "AnnotationLibrary" "0.4.0"]
                 ["org.aspectj" "aspectjrt" "1.6.11"]
                 ["log4j" "log4j" "1.2.15" "dev"]
                 ["commons-logging" "commons-logging" "1.1.1" "dev"]
