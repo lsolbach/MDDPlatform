a44e8a1300da2d4ca53870b186dba275ccaf21d0
Ludger Solbach <soulman@soulspace.org>
2013-07-23 00:08:08 +0200

    refactored module.clj, fixed classpath

--- module.clj
+++ module.clj
@@ -1,13 +1,14 @@
 [
- :name "JmxLibrary"
+ :module "JmxLibrary"
  :project "org.soulspace.architecture"
  :type "library"
  :version "0.2.0"
  :description "A library for managing application with the Java Management Extension (JMX)."
  :vendor "soulspace.org"
- :plugins ["global" "deps" "java" "package"]
- :dependencies [["org.soulspace.architecture" "AspectLibrary" "0.2.0"]
-                ["org.soulspace.architecture" "AnnotationLibrary" "0.4.0"]
-                ["log4j" "log4j" "1.2.15" "dev"]
-                ["junit" "junit" "3.8.1" "dev"]]
+ :plugins ["global" "sdeps" "aspectj" "package"]
+ :dependencies [[["org.soulspace.architecture" "AspectLibrary" "0.2.0"] "aspect"]
+                [["org.soulspace.architecture" "AnnotationLibrary" "0.4.0"]]
+                [["org.aspectj" "aspectjrt" "1.6.11"]]
+                [["log4j" "log4j" "1.2.15"] "dev"]
+                [["junit" "junit" "3.8.1"] "dev"]]
  ]
