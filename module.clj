a6bf85fa4887539f75b84e9c1811b130a21297fe
Ludger Solbach <soulman@soulspace.org>
2013-07-30 21:24:11 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -2,11 +2,11 @@
  :module "JavaUtilLibrary"
  :project "org.soulspace.common"
  :project-lead "Ludger Solbach"
- :vendor "soulspace.org"
- :type "library"
+ :provider "soulspace.org"
+ :type :library
  :version "0.3.0"
  :description "The JavaUtilLibrary is a library of utility classes with pure java convenience methods."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :plugins ["global" "sdeps" "java" "junit" "package"]
- :dependencies [[["junit" "junit" "3.8.1"] "dev"]]
+ :dependencies [[["junit" "junit" "3.8.1"] :dev]]
  ]
