19ecb6c8cb9c84fde9b39a34dfac0d589956725d
Ludger Solbach <soulman@soulspace.org>
2013-07-30 21:28:10 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -1,10 +1,10 @@
 [
  :module "JpaLibrary"
  :project "org.soulspace.architecture"
- :type "library"
+ :type :library
  :version "0.3.0"
  :description "A repository for UML 1.4/XMI 1.2 files"
- :vendor "soulspace.org"
+ :provider "soulspace.org"
  :plugins ["global" "sdeps" "java" "package"]
  :dependencies [[["org.springframework" "spring-orm" "3.0.5.RELEASE"]]]
  ]
