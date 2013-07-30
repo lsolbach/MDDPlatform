949a61cf494419fa7676831733d24d699a47c4a6
Ludger Solbach <soulman@soulspace.org>
2013-07-30 21:21:20 +0200

    refactored module.clj

--- module.clj
+++ module.clj
@@ -2,7 +2,7 @@
  :module "AnnotationLibrary"
  :project "org.soulspace.architecture"
  :project-lead "Ludger Solbach"
- :type "library"
+ :type :library
  :version "0.4.0"
  :description "A java annotation library to support model driven and aspect oriented architectures."
  :vendor "soulspace.org"
