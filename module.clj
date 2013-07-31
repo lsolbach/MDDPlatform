f35f1a60bb5470b004e5553f0f46786903027a50
Ludger Solbach <soulman@soulspace.org>
2013-07-31 09:06:11 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -7,6 +7,6 @@
  :version "0.3.0"
  :description "The JavaUtilLibrary is a library of utility classes with pure java convenience methods."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "sdeps" "java" "junit" "package"]
+ :plugins ["global" "sdeps" "depsdot" "java" "junit" "package"]
  :dependencies [[["junit" "junit" "3.8.1"] :dev]]
  ]
