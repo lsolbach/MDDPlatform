6749c7bffccb832de82cdb3728e10a9f516d4af3
Ludger Solbach <soulman@soulspace.org>
2013-08-03 07:20:22 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -6,7 +6,7 @@
  :description "A library for managing application with the Java Management Extension (JMX)."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :provider "soulspace.org"
- :plugins ["global" "sdeps" "aspectj" "package"]
+ :plugins ["global" "dependencies" "aspectj" "junit" "package"]
  :dependencies [[["org.soulspace.architecture" "AspectLibrary" "0.2.0"] :aspect]
                 [["org.soulspace.architecture" "AnnotationLibrary" "0.4.0"]]
                 [["log4j" "log4j" "1.2.15"] :dev]
