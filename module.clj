6a775cd4a5da325deb511076455d4df386f752c2
Ludger Solbach <soulman@soulspace.org>
2013-08-03 07:47:02 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :project-lead "Ludger Solbach"
  :type :library
  :version "0.4.0"
- :description "A java annotation library to support model driven and aspect oriented architectures."
+ :description "A java annotation library to support domain driven, model driven and aspect oriented architectures."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :provider "soulspace.org"
  :plugins ["global" "dependencies" "java" "package"]
