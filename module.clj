59b6ce67c858a6655a7087678a6f119163128492
Ludger Solbach <soulman@soulspace.org>
2013-08-03 07:13:41 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -6,7 +6,7 @@
  :version "0.2.0"
  :description "An architecture framework with abstractions and aspects"
  :vendor "soulspace.org"
- :plugins ["global" "sdeps" "depsdot" "aspectj" "junit" "package"]
+ :plugins ["global" "dependencies" "aspectj" "junit" "package"]
  :dependencies [[["org.soulspace.architecture" "AspectLibrary" "0.2.0"] :aspect]
                 [["org.soulspace.architecture" "AnnotationLibrary" "0.4.0"]]
                 [["junit" "junit" "3.8.1"] :dev]]
