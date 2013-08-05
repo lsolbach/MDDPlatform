f35ca4f9eb5758f3d4c28cd060a3df6e8ce0760e
Ludger Solbach <soulman@soulspace.org>
2013-08-05 03:13:01 +0200

    updated module files, added copyright notice in source files

--- module.clj
+++ module.clj
@@ -3,7 +3,7 @@
  :project "org.soulspace.architecture"
  :type :library
  :version "0.2.0"
- :description "A library for managing application with the Java Management Extension (JMX)."
+ :description "A library of aspects for managing applications with the Java Management Extension (JMX)."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
  :provider "soulspace.org"
  :plugins ["global" "dependencies" "aspectj" "junit" "package"]
