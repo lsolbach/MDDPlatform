9a49da2cd6728bc7bddc62d74b9eecdf62740053
soulman <soulman@pluto>
2014-11-11 21:33:08 +0100

    removed date utils, updated module.clj

--- module.clj
+++ module.clj
@@ -5,11 +5,15 @@
  :provider "soulspace.org"
  :inceptionYear 2003
  :type :library
- :version "0.3.0"
+ :version "0.3.1"
  :description "The JavaUtilLibrary is a library of utility classes with pure java convenience methods."
  :license ["Eclipse Public License 1.0" "http://www.eclipse.org/legal/epl-v10.html"]
- :plugins ["global" "dependencies" "java" "junit" "package"]
- :dependencies [[["junit" "junit" "3.8.1"] :dev]]
+ :plugins ["global"
+           ["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/JavaPlugin"]
+           ["org.soulspace.baumeister/JUnitPlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["junit/junit, 3.8.1" :dev]]
  :java-javadoc-windowtitle "${module} ${version}"
  :java-javadoc-doctitle "${module} ${version}"
  :java-javadoc-header "<FONT CLASS=\"NavBarFont1\">${module} ${version}</FONT>"
